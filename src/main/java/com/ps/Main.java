5a768a0fd1ea014c8db531a87f06e9295790686c
Ivan Garcia <iegarcia611@gmail.com>
2024-10-11 12:11:24 -0500

    created transaction class with constructor/getters and setters and created initial menu with methods to test functionallity

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,8 +1,51 @@
 package com.ps;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("Hello world!");
+public class Main {
+
+    static Scanner commandScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // loadLedgerFile(); <-- this will load the files as soon as the app is ran, if i dont then option 3 will be empty
+
+        int mainMenuCommand;
+        do{
+            System.out.println("1) to Add Deposit");
+            System.out.println("2) to  Make Payment(debit)");
+            System.out.println("3) to Display Ledger");
+            System.out.println("Command: ");
+            mainMenuCommand = commandScanner.nextInt();
+
+            switch(mainMenuCommand){
+                case 1:
+                    addDeposit();
+                    break;
+                case 2:
+                    makePayment();
+                    break;
+                case 3:
+                    displayLedger();
+                    break;
+                case 0:
+                    System.out.println("Exiting the application. . .");
+                    break;
+                default:
+                    System.out.println("Invalid command try again. . .");
+            }
+
+        }while(mainMenuCommand != 0);
+    }
+
+    public static void addDeposit(){
+        System.out.println("Adding deposit test. . .");
+    }
+
+    public static void makePayment(){
+        System.out.println("make payment test. . .");
+    }
+
+    public static void displayLedger(){
+        System.out.println("display ledger test. . .");
     }
 }
---
+++ src/main/java/com/ps/Transaction.java
@@ -0,0 +1,71 @@
+package com.ps;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Transaction {
+    private String description;
+    private String vendor;
+    private double amount;
+    private LocalDate date;
+    private LocalTime time;
+
+    public Transaction(String description, String vendor, double amount, LocalDate date, LocalTime time) {
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+        this.date = date;
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalTime time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                ", date=" + date +
+                ", time=" + time +
+                '}';
+    }
+}
